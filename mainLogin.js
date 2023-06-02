e691f9718c80f94cd089d10c7eed74cd7e654b55
lalex <lalex@gmail.com>
2023-06-02 07:39:43 -0600

    ffjffj

---
+++ mainLogin.js
@@ -0,0 +1,74 @@
+
+
+
+const registrarUsuario=async()=>{
+ var correo=document.querySelector("#correo").value;
+ var password=document.querySelector("#password").value;
+ var nombre=document.querySelector("#nombre").value;
+
+
+ if(correo.trim()===''||
+ password.trim()===''||
+ nombre.trim()===''){
+
+    Swal.fire({
+        icon: 'error',
+        title: 'ERROR...',
+        text: 'FALTA LLENAR CAMPOS',
+        footer: 'CRUD CONTACTOS'
+      })
+ }
+
+ if( !validarCorreo(correo)){
+
+    Swal.fire({
+        icon: 'error',
+        title: 'ERROR...',
+        text: 'INTRODUCE UN CORREO ELECTRÓNICO VÁLIDO',
+        footer: 'CRUD CONTACTOS'
+      })
+     return;
+    }
+  
+  
+ if( !validarPassword(password)){
+
+  Swal.fire({
+      icon: 'error',
+      title: 'ERROR...',
+      text: 'INTRODUCE UNA CONTRASEÑA VÁLIDA [Mayúsculas,minúsculas, números y mínimo 8 caracteres] ',
+      footer: 'CRUD CONTACTOS'
+    })
+   return;
+  }
+
+
+if( !validarNombre(nombre)){
+
+  Swal.fire({
+      icon: 'error',
+      title: 'ERROR...',
+      text: 'INTRODUCE UN NOMBRE VALIDO [Mayúsculas,minúsculas y números] ',
+      footer: 'CRUD CONTACTOS'
+    })
+   return;
+  }
+  //INSERTAR A LA BASE DE DATOSSSSSSSSSSSSSS :))))))
+  const datos=new FormData();
+  datos.append("correo",correo);
+  datos.append("password",password);
+  datos.append("nombre",nombre);
+
+var respuesta=await fetch ("php/Usuario/registrarUsuario.php",{
+   method: 'POST',
+   body:datos
+});
+Swal.fire({
+  icon: 'success',
+  title: 'EXITO',
+  text: 'RESPUESTA '+ respuesta,
+  footer: 'CRUD CONTACTOS'
+})
+
+
+}
---
+++ php/Usuario/registrarUsuario.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once '../config.php';
+$valido['success']=array('success'=>false, 'mensaje' =>"");
+
+if($_POST){
+$correo=$_POST['correo'];
+$password=md5($_POST['correo']);
+$nombre=$_POST['nombre'];
+
+$sql="SELECT * FROM usuario WHERE   correo='$correo'";
+$resultado=$cx->query($sql);
+$n=$resultado->num_rows;
+if($n==0){
+    $sqlInsertar="INSERT INTO usuario VALUES(null,'$correo','$password','$nombre')";
+    if($cx->query($sqlInsertar)==true){
+        $valido['success']=true;
+        $valido['mensaje']="SE GUARDÓ CORRECTAMENTE";
+    }else{
+        $valido['success']=false;
+        $valido['mensaje']="ERROR:NO SE GUARDÓ";
+    }
+}else{
+    $valido['success']=false;
+    $valido['mensaje']="EL CORREO ELECTRÓNICO YA ESTÁ EN USO";
+}
+
+}else{
+    $valido['success']=false;
+    $valido['mensaje']="NO SE GUARDÓ";
+}
+
+?>
